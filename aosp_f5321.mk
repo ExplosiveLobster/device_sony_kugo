d1be6aedff28e61495d91e52eefe04a68af1eb83
Pavel Dubrova <pashadubrova@gmail.com>
2020-12-30 09:17:16 +0200

    aosp_f5321: Update kernel defconfig name
    We don't use aosp_ prefix in our defconfig files.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- aosp_f5321.mk
+++ aosp_f5321.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_KERNEL_CONFIG := aosp_loire_kugo_defconfig
+KERNEL_DEFCONFIG := loire_kugo_defconfig
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/kugo/device.mk)
